f9e9cee9fb266a7d0727a4f2187da532c951b016
wangfei <wangfei@ethercap.com>
2017-01-09 10:30:54 +0800

    增加提交到maven配置代码

--- QiaoBa/secondmodule/src/main/java/com/gavin/secondmodule/router/IRouterUri.java
+++ QiaoBa/secondmodule/src/main/java/com/gavin/secondmodule/router/IRouterUri.java
@@ -8,8 +8,9 @@ import com.protocol.annotation.router.RouterUri;
  */
 
 public interface IRouterUri {
+    @RouterUri("xl://main:8888/demo")//此注解对应的是 url的地址
+    public void jumpToDemo(@RouterParam("key") String key);//RouterParam 对应的参数，里面值是参数名
 
-    @RouterUri("xl://main:8888/demo")
-    public void jumpToDemo(@RouterParam("key") String key);
+    //如果增加页面，可以在这里增加对应的方法即可
 
 }
